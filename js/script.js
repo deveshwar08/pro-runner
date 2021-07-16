81a1b635a8278ad06f20b2d27f3e74d45c9fe780
deveshwar08 <76254901+deveshwar08@users.noreply.github.com>
2021-07-16 11:41:17 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var runner = null;
+var runner;
 var canvas = document.createElement("canvas");
 var gameArea = document.getElementById("game-area");
 let c = canvas.getContext('2d');
@@ -10,19 +10,76 @@ let holeSpeed = 5;
 let timeGap = [1,2,3];
 let time = timeGap[Math.round(Math.random() * timeGap.length)];
 let currentTime = 0;
+let score = 0;
+let playerName;
+let gameOver = false;
 
 gameArea.appendChild(canvas);
 canvas.height = gameArea.clientHeight;
 canvas.width = gameArea.clientWidth;
 
+function displayScore()
+{
+    document.getElementById("score-area").innerHTML = "";
+    if(localStorage.getItem("HighScore") == null)
+    {
+        return true;
+    }
+    let retrievedHighScore = JSON.parse(localStorage.getItem("HighScore"));
+    let h1 = document.createElement("h1");
+    let highScoreText = document.createTextNode("HIGH SCORE")
+    h1.appendChild(highScoreText);
+    let h21 = document.createElement("h2");
+    let highScoreName = document.createTextNode("Name : " + retrievedHighScore[0]);
+    let h22 = document.createElement("h2");
+    let highScoreScore = document.createTextNode("Score : " + +(Math.round(retrievedHighScore[1] + "e+2")  + "e-2"))
+    h21.appendChild(highScoreName);
+    h22.appendChild(highScoreScore);
+    document.getElementById("score-area").appendChild(h1);
+    document.getElementById("score-area").appendChild(h21);
+    document.getElementById("score-area").appendChild(h22);
+}
+function closePopup(){
+    document.getElementById("overlay").classList.remove("active");
+    document.getElementById("score-popup").classList.remove("active");
+    document.getElementById("score-popup-body").innerText = ""; 
+    location.reload();
+    location.reload();
+}
+
+function popUpScore(){
+    document.getElementById("overlay").classList.add("active");
+    document.getElementById("score-popup").classList.add("active");
+    document.getElementById("score-popup-body").innerText = "Score : " + +(Math.round(score + "e+2")  + "e-2"); 
+    let highScore = [];
+    highScore[0] = playerName;
+    highScore[1] = +(Math.round(score + "e+2")  + "e-2");
+    if(localStorage.getItem("HighScore") == null)
+        localStorage.setItem("HighScore",JSON.stringify(highScore));
+    else{
+        let retrievedHighScore = JSON.parse(localStorage.getItem("HighScore"));
+        if(retrievedHighScore[1] <= +(Math.round(score + "e+2")  + "e-2"))
+        {
+            localStorage.removeItem("HighScore");
+            localStorage.setItem("HighScore",JSON.stringify(highScore));
+        }
+    }
+}
+function calculateScore(){
+    score += 0.01;
+    t = setTimeout(function() {
+        calculateScore();
+    }, 10);
+}
+
 function createPlatform() {
-    c.fillStyle = 'rgba(0,0,0,1)';
+    c.fillStyle = 'rgba(22,22,22,255)';
     c.fillRect(0, 0, canvas.width, platformHeight);
     c.fillRect(0, canvas.height - platformHeight, canvas.width, platformHeight);
 }
 
 function createRunner() {
-    c.fillStyle = 'rgba(0,0,255,1)';
+    c.fillStyle = 'rgba(37,136,212,255)';
     if(playerPosition == "up")
         c.fillRect(0, platformHeight, playerSize, playerSize);
     else
@@ -39,11 +96,11 @@ function updateRunner() {
 function UpperHole(){
     this.x = canvas.width;
     this.draw = function(){
-    c.fillStyle = 'rgba(255,255,255,1)';
+    c.fillStyle = 'rgba(54,54,54,255)';
     c.fillRect(this.x, 0, holeWidth, platformHeight);
     }
     this.update = function(){
-        c.fillStyle = 'rgba(0,0,0,1)';
+        c.fillStyle = 'rgba(22,22,22,255)';
         c.fillRect(this.x, 0, holeWidth, platformHeight);
         this.x -= holeSpeed;
         this.draw();
@@ -52,11 +109,11 @@ function UpperHole(){
 function LowerHole(){
     this.x = canvas.width;
     this.draw = function(){
-    c.fillStyle = 'rgba(255,255,255,1)';
+    c.fillStyle = 'rgba(54,54,54,255)';
     c.fillRect(this.x, canvas.height - platformHeight, holeWidth, platformHeight);
     }
     this.update = function(){
-        c.fillStyle = 'rgba(0,0,0,1)';
+        c.fillStyle = 'rgba(22,22,22,255)';
         c.fillRect(this.x, canvas.height - platformHeight, holeWidth, platformHeight);
         this.x -= holeSpeed;
         this.draw();
@@ -71,12 +128,11 @@ function switchSide(){
     updateRunner();
 }
 function updateTime() {
-    currentTime += 0.1;
+    currentTime ++;
     if(currentTime >= time)
     {
         currentTime = 0;
         time = timeGap[Math.round(Math.random() * (timeGap.length-1))];
-        console.log(time + "," + currentTime);
         let side = [0,1];
         let holeTemp;
         if(side[Math.round(Math.random() * 2)] == 1)
@@ -86,10 +142,11 @@ function updateTime() {
         holeTemp.draw();
         animateHole(holeTemp);
     }
-
-    t = setTimeout(function() {
-        updateTime();
-    }, 100);
+    if(gameOver == false){
+        t = setTimeout(function() {
+            updateTime();
+        }, 1000);
+    }
 }
 
 function animateHole(hole)
@@ -99,10 +156,16 @@ function animateHole(hole)
         {
             if(hole.x > -holeWidth)
             {    
-                requestAnimationFrame(animate);
-                hole.update();
                 if(isFallen(hole))
-                    alert("You fell");
+                {
+                    playerName = prompt("What's your name?");
+                    gameOver = true;
+                    popUpScore();
+                }
+                if(gameOver == false){
+                    requestAnimationFrame(animate);
+                    hole.update();
+                }
             }
         }
 }
@@ -115,6 +178,11 @@ function isFallen(hole)
     }
     return false;
 }
+document.body.addEventListener('keydown', function(e) {
+  if(/*e.keyCode == 32 &&*/ e.target == document.body) {
+    e.preventDefault();
+  }
+});
 
 document.body.onkeyup = function(e){
     if(e.keyCode == 32)
@@ -122,4 +190,6 @@ document.body.onkeyup = function(e){
 }
 createRunner();
 createPlatform();
+displayScore();
+calculateScore();
 updateTime();
